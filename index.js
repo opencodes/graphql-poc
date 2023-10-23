f192363dc119bf45eddd7dd29b4825c1effb037d
homepc <rajesh.jha@konze.com>
2023-10-23 06:30:43 +0530

    Apollo Graphql

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-import express from "express";
-import { graphqlHTTP } from "express-graphql";
+// import express from "express";
+// import { graphqlHTTP } from "express-graphql";
 import schema from "./app/data/schema";
 import { resolvers } from "./app/data/resolvers";
+import { ApolloServer } from "apollo-server";
 
-const app = express();
+// const app = express();
 
-app.get("/", (req, res) => {
-  res.send("GraphQL is amazing!");
+// app.get("/", (req, res) => {
+//   res.send("GraphQL is amazing!");
+// });
+// const root = resolvers;
+
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema: schema,
+//     rootValue: root,
+//     graphiql: true,
+//   })
+// );
+
+// app.listen(4000, () => {
+//   console.log("Running a GraphQL API server at localhost:4000");
+// });
+const server = new ApolloServer({
+  schema,
+  mocks: true,
 });
-const root = resolvers;
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
-
-app.listen(4000, () => {
-  console.log("Running a GraphQL API server at localhost:4000");
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
